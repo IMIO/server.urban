c6b93173f13c962e63ab0212ac092838d3ff1bfa
Simon Delcourt <delcourt.simon@gmail.com>
2018-06-06 14:12:03 +0200

    Try to luanch libreoffice in server mode.

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ if [[ "$1" = "zeo"* ]]; then
   CMD="bin/$1 fg"
 fi 
 if [[ "$1" = "instance"* ]]; then
+  soffice -invisible -headless "-accept=socket,host=localhost,port=2002;urp;" fg
   CMD="bin/$1 fg"
-  echo "LAUNCH LIBREOFFICE"
 fi
 gosu imio $CMD
