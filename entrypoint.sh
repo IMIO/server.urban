ebc67256a5fe2da3fc4033efea88be3cdc2b90be
Simon Delcourt <delcourt.simon@gmail.com>
2018-06-06 15:19:05 +0200

    Fix openoffice launch.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -e
-CMD="unknown command"
+CMD="echo unknown command"
 cd $URBAN_DIR
 
 if [[ "$1" = "zeo"* ]]; then
   CMD="bin/$1 fg"
 fi 
 if [[ "$1" = "instance"* ]]; then
-  soffice -invisible -headless "-accept=socket,host=localhost,port=2002;urp;" fg
+  soffice --invisible --headless "--accept=socket,host=localhost,port=2002;urp;" &
   CMD="bin/$1 fg"
 fi
 gosu imio $CMD
